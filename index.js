4a91c4b9984ba3eeac60c3e07a1e1c7efba5feea
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-31 22:45:56 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    const hq = 42
+    if (blocks >= 42){
+        return blocks - hq
+    }else (blocks < 42)
+        return hq - blocks
+    
+} function distanceFromHqInFeet(blocks){
+    distanceFromHqInBlocks(blocks)
+    return distanceFromHqInBlocks(blocks) * 264
+} 
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264
+} 
+function calculatesFarePrice(start, destination){
+  const feet = (Math.abs(destination - start) * 264)
+    if (feet < 400){
+        return 0
+    }else if (feet <= 2000) {
+        return (feet - 400) * .02 
+    }else if (feet <= 2500){
+        return 25
+    }else (feet > 2500)
+    return "cannot travel that far"
+}
